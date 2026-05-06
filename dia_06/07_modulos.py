6d429e50a2530c01a7732dd5f88c6fa5cc851bc6
farias741 <deborafarias297@gmail.com>
2026-05-06 06:57:10 -0300

    aula 06 - modulos/aleatorio

---
+++ dia_06/07_modulos.py
@@ -0,0 +1,29 @@
+# %%
+import math # assim eu chamo a biblioteca inteira
+from math import pi, e # e assim eu importo apenas o pi, e
+# from math import * # não é recomendado
+import math as mh
+
+
+# %%
+print(mh.factorial(4))
+
+
+math.sqrt(9)
+
+# %%
+
+math.pow(2, 4)
+
+# %%
+
+math.e
+
+# %%
+
+math.pi
+
+# %%
+
+pi
+
---
+++ dia_06/08_aleatorio.py
@@ -0,0 +1,19 @@
+# %%
+
+# Importando a biblioteca
+
+import random
+
+# %%
+
+# Gerando um número aleatório
+
+random.randint(1,10) 
+
+# %%
+
+# Escolhendo aleatoriamente um elemento da lista
+
+x = ["Téo", "Maria", "Jose", "Ana", "Joao"]
+
+random.choice(x)
